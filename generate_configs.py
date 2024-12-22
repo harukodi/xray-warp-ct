b8ad324c66d62b404f129f3d6ccb6c41fec9fbad
xia1997x <nikolamihov2@gmail.com>
2024-12-22 16:22:52 +0000

    converted from grpc to xhttp due to deprecation of grpc

--- generate_configs.py
+++ generate_configs.py
@@ -4,7 +4,7 @@ from warp_class import Warp
 from urllib.parse import quote
 
 xray_uuid = None
-xray_servicename = None
+xray_path = None
 
 def load_xray_json_data():
     global xray_json_data
@@ -17,15 +17,15 @@ def load_warp_json_data():
         warp_json_data = json.load(warp_config)
 
 def generate_random_string_and_uuid():
-    global xray_uuid, xray_servicename
+    global xray_uuid, xray_path
     random_xray_uuid = str(uuid.uuid4())
-    random_xray_servicename = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=38))
+    random_xray_path = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=38))
     xray_uuid = random_xray_uuid
-    xray_servicename = random_xray_servicename
+    xray_path = random_xray_path
 
-def generate_xray_qr_code(uuid, domain, port, service_name, fingerprint):
+def generate_xray_qr_code(uuid, domain, port, xhttp_path, fingerprint):
     encoded_remark = quote(domain, safe="")
-    vless_uri = f"vless://{uuid}@{domain}:{port}?encryption=none&security=tls&sni={domain}&alpn=h3%2Ch2%2Chttp%2F1.1&fp={fingerprint}&type=grpc&authority={domain}&serviceName={service_name}&mode=gun#xia1997x: {encoded_remark}"
+    vless_uri = f"vless://{uuid}@{domain}:{port}?encryption=none&security=tls&sni={domain}&alpn=h3%2Ch2%2Chttp%2F1.1&fp={fingerprint}&type=xhttp&host={domain}&path={xhttp_path}&mode=auto#{encoded_remark}"
     qr_code = segno.make_qr(vless_uri)
     qr_code.save("./xray_config/xray_client_qr_code.png", scale=8)
 
@@ -33,11 +33,11 @@ def generate_xray_config():
     load_xray_json_data()
     load_warp_json_data()
     generate_random_string_and_uuid()
-    generate_xray_qr_code(xray_uuid, domain_name, port, xray_servicename, fingerprint)
+    generate_xray_qr_code(xray_uuid, domain_name, port, xray_path, fingerprint)
     XRAY_SETTINGS_UUID = xray_json_data['inbounds'][0]['settings']['clients'][0]
     XRAY_SETTINGS_UUID['id'] = xray_uuid
-    XRAY_STREAMSETTINGS_SERVICENAME = xray_json_data['inbounds'][0]['streamSettings']['grpcSettings']
-    XRAY_STREAMSETTINGS_SERVICENAME["serviceName"] = xray_servicename
+    XRAY_STREAMSETTINGS_XHTTP = xray_json_data['inbounds'][0]['streamSettings']['xhttpSettings']
+    XRAY_STREAMSETTINGS_XHTTP['path'] = xray_path
     warp_addresses = [Warp.warp_ipv4(), Warp.warp_ipv6()]
     warp_json_data['settings']['secretKey'] = Warp.warp_private_key()
     warp_json_data['settings']['address'] = warp_addresses
@@ -66,10 +66,10 @@ def generate_caddy_config():
     caddy_tls_subject['subjects'] = f"{domain_name}"
     # Sets the proxy path to the service
     caddy_proxy_path = caddy_json_data['apps']['http']['servers']['srv0']['routes'][0]['handle'][0]['routes'][0]['handle'][0]['routes'][0]['match'][0]
-    caddy_proxy_path['path'] = f"/{xray_servicename}/*"
+    caddy_proxy_path['path'] = f"/{xray_path}/*"
     # Sets the match path for the handle
     caddy_match_path = caddy_json_data['apps']['http']['servers']['srv0']['routes'][0]['handle'][0]['routes'][0]['match'][0]
-    caddy_match_path['path'] = f"/{xray_servicename}/*"
+    caddy_match_path['path'] = f"/{xray_path}/*"
     # Saves the dynamic config file
     with open("./caddy_config/caddyfile.json", "w") as caddyfile:
         json.dump(caddy_json_data, caddyfile, indent=4)
