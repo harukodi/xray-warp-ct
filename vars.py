6647034036f842b6e6668257a856da1163ee3340
xia1997x <nikolamihov2@gmail.com>
2024-12-22 16:26:08 +0000

    Changes to the vars file removed default for the env vars

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 from os import environ
-domain_name = environ.get("DOMAIN_NAME", "example.com")
-port = environ.get("PORT", "4444")
+domain_name = environ.get("DOMAIN_NAME")
+port = environ.get("PORT", "443")
 fingerprint = environ.get("FINGERPRINT", "chrome")
 xray_version = environ.get("XRAY_VERSION", "latest")
 wgcf_version = environ.get("WGCF_VERSION", "latest")
