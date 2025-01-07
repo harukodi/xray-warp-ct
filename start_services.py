676547cb4340770eb2725fd0082de2c007aa459a
xia1997x <nikolamihov2@gmail.com>
2025-01-07 21:46:54 +0000

    Minor fix

--- start_services.py
+++ start_services.py
@@ -3,9 +3,9 @@ from vars import enable_caddy_log
 
 def start_caddy_server():
     if enable_caddy_log.lower() == "true":
-        return subprocess.Popen(["caddy", "run", "--config", "./caddy_config/caddyfile.json"], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-    else:
         return subprocess.Popen(["caddy", "run", "--config", "./caddy_config/caddyfile.json"])
+    else:
+        return subprocess.Popen(["caddy", "run", "--config", "./caddy_config/caddyfile.json"], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
 
 def start_xray_core():
     return subprocess.Popen(["./xray_core/xray", "run", "-c", "./xray_config/xray_config.json"])
