d79979b196513aa191732d5061678c5dbdc4d5db
xia1997x <nikolamihov2@gmail.com>
2025-01-07 21:36:21 +0000

    added the ability to enable or disable the caddy log output

--- start_services.py
+++ start_services.py
@@ -1,7 +1,11 @@
 import subprocess
+from vars import enable_caddy_log
 
 def start_caddy_server():
-    return subprocess.Popen(["caddy", "run", "--config", "./caddy_config/caddyfile.json"])
+    if enable_caddy_log.lower() == "true":
+        return subprocess.Popen(["caddy", "run", "--config", "./caddy_config/caddyfile.json"], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+    else:
+        return subprocess.Popen(["caddy", "run", "--config", "./caddy_config/caddyfile.json"])
 
 def start_xray_core():
     return subprocess.Popen(["./xray_core/xray", "run", "-c", "./xray_config/xray_config.json"])
