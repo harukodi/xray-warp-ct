b315d959fc570def244715537bf3e49c62703885
xia1997x <nikolamihov2@gmail.com>
2025-10-23 22:11:16 +0000

    testing xray connectivity check workflow

--- tests/infrastructure/xray_server_config/setup-xray-server.sh
+++ tests/infrastructure/xray_server_config/setup-xray-server.sh
@@ -11,7 +11,7 @@ declare -A xray_config_values=(
 function install_docker_tools () {
     # Add Docker's official GPG key
     sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt-get install ca-certificates curl -y
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -22,7 +22,7 @@ function install_docker_tools () {
       $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 }
 
 function copy_config_from_template_func () {
